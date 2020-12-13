170d65bb4d4bfe2d62795608fed626e3ece96a8d
ouali <ouali.aghiles@gmail.com>
2020-12-13 14:50:54 +0100

    complete the quick view modal and change the event from image to icon on product

--- src/assets/scripts/js/script.js
+++ src/assets/scripts/js/script.js
@@ -158,8 +158,52 @@ $('.quick-btn').on('click', function(){
       autoStart: true
     }
   })
+
+  inputNumber($('.quantity'));
 })
 
 
+var inputNumber = function(el) {
+
+  var min = el.attr('min') || false;
+  var max = el.attr('max') || false;
+
+  var els = {};
+
+  els.dec = el.prev();
+  els.inc = el.next();
+
+  el.each(function() {
+    init($(this));
+  });
+
+  function init(el) {
+
+    els.dec.on('click', decrement);
+    els.inc.on('click', increment);
+
+    function decrement() {
+      var value = el[0].value;
+      value--;
+      if(!min || value >= min) {
+        el[0].value = value;
+      }
+    }
+
+    function increment() {
+      var value = el[0].value;
+      value++;
+      if(!max || value <= max) {
+        el[0].value = value++;
+      }
+    }
+  }
+}
 
 
+
+document.querySelectorAll('.wish').forEach((el) => {
+  el.addEventListener('click', () => {
+    document.querySelector('.toast').show()
+  })
+});
